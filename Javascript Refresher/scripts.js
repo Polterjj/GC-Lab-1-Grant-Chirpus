782cf1fe011a821a9a0455cc6a332b807a09d7d6
JJ <jjpdet@gmail.com>
2022-10-13 16:18:53 -0400

    Javascript Refresher

---
+++ Javascript Refresher/scripts.js
@@ -0,0 +1,26 @@
+let firstName = "JJ";
+let lastName = "Polter";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let pop = 675000;
+
+console.log(pop);
+
+pop = pop * 3;
+
+console.log(pop);
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for (let i = 0; i <= 5; i++){
+    console.log(i);
+}
+
+for (let i = 1; i <= 4; i++){
+    console.log(i);
+}
+
