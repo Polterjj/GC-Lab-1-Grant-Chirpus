e92a1bb070a7f362bfd8ba5938fe3806e4db67fe
JJ <jjpdet@gmail.com>
2022-10-16 17:44:10 -0400

    JS Heating cooling

---
+++ Scratch/Lab 3 (Temperatures)/scripts.js
@@ -0,0 +1,22 @@
+let actualTemp = 64;
+
+let desiredTemp = 69;
+
+if (actualTemp > desiredTemp) {
+    console.log("Run A/C")
+}
+else if (actualTemp < desiredTemp){
+    console.log("Run Heat")
+}
+else {
+    console.log("Standby")
+}
+
+
+let tempCelsius = 32;
+
+let targetUnit = "K";
+
+if (targetUnit = "K"){
+    console.log ('The temperature is' (tempCelsius + 273.15), 'degrees Kelvin')
+} 
